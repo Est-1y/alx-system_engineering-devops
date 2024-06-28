e7518c3978166b058af80e275182b4c5ee2ebc1a
Est-1y <terryiana22@gmail.com>
2024-06-28 16:22:22 +0000

    1-install_a_package.pp

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Installing flask from pip3 version 2.1.0
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
