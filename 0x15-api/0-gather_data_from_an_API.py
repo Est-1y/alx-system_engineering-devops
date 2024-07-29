7e36030587f51d85b9a46e9d93d7694999b56eff
Est-1y <terryiana22@gmail.com>
2024-07-29 15:54:31 +0000

    gather data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
+    # progress calcution
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
           len(completed), len(todos)))
