d0840cdcac7a045f78ca2041d8d3d60206365e8e
Est-1y <terryiana22@gmail.com>
2024-07-29 13:42:29 +0000

    gather data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,50 +1,48 @@
 #!/usr/bin/python3
-"""
-retrieving and displayimg TODO list progress for a given employee
-"""
+"""Retrieving and displaying TODO list progress for a given employee."""
 
 import requests
 import sys
 
 
 def get_employee_todo_progress(employee_id):
-    """ using REST API for a given employee ID
-    """
+    """Using REST API for a given employee ID."""
     base_url = "https://jsonplaceholder.typicode.com"
     user_url = "{}/users/{}".format(base_url, employee_id)
     todos_url = "{}/todos?userId={}".format(base_url, employee_id)
 
-    # user info
+    # User info.
     user_response = requests.get(user_url)
     user_data = user_response.json()
     employee_name = user_data.get('name')
 
-    # TODO list
+    # TODO list.
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
 
-    # Progress
+    # Progress check
     total_tasks = len(todos_data)
     completed_tasks = sum(task.get("completed", False) for task in todos_data)
 
-    # Progress info.
+    # Progress info
     print("Employee {} is done with tasks ({}/{}):".format(
-        employee_name, completed_tasks, total_tasks), end='\n')
+        employee_name, completed_tasks, total_tasks))
 
-    # Titles of tasks done.
+    # Titles of tasks done
     for task in todos_data:
         if task.get('completed', False):
             print("\t {}".format(task.get("title")))
 
 
 if __name__ == "__main__":
-    # verify command-line arguments
+    # Verify command-line arguments
     if len(sys.argv) != 2:
         print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    # Employee ID.
+    # Employee ID
     employee_id = int(sys.argv[1])
 
-    # function getting TODO list progress.
+    # Get TODO list progress
     get_employee_todo_progress(employee_id)
+
