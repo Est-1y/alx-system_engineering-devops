91358d207a5eaf674a1e3d9e94b599baf898bfb6
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-08-13 22:02:42 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Querying subscribers on Reddit subreddit."""
+"""Querying subscribers on a Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    headers = {'User-Agent': 'RedditDataAnalyzer/1.0 (ALX Africa)'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-
-        subscribers = data.get('data', {}).get('subscribers', 0)
-
-        return subscribers
-    else:
-        return 0
+    try:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
+            (by /u/Large_Alternative_30)"
+        }
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 404:
+            return 0
+        results = response.json().get("data")
+        return results.get("subscribers")
+    except (Exception):
+        print('Not Found')
+        return (0)
