40756a176ebceb42976867d9c2d200a82d40a17c
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-08-13 18:47:51 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 """
-number_of_subscribers function for a given
-subreddit
+function querying the Reddit API.
 """
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """querying Reddit API"""
+    """querying Reddit API and returning no. of subs."""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
+    
+    try:
+        r = requests.get(url, headers=headers)
+        if r.status_code != 200:
+            return 0
+        r_json = r.json()
+        subs = r_json.get("data", {}).get("subscribers", 0)
+        return subs
+    except requests.exceptions.RequestException as e:
+        return 0
+    except ValueError:
+        return 0
