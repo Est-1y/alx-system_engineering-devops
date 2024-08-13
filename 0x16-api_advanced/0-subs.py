b995f5209ed9c4fe87675388dd85d6db5336cfc0
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-08-13 18:24:49 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,7 @@
 And 0 if invalid
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
@@ -11,12 +11,16 @@ def number_of_subscribers(subreddit):
     Queries.
     """
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "linux:0x16.api.advanced"}
-    response = requests.get(url=url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 404:
+   if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    results = response.json().get("data")
-    return results.get("subscribers")
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
