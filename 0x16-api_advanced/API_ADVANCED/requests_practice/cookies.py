af3b1bbc674db2e2c4760dfc21ef4fb6d76f7ee0
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-08-13 21:21:57 +0300

    Update cookies.py

--- 0x16-api_advanced/API_ADVANCED/requests_practice/cookies.py
+++ 0x16-api_advanced/API_ADVANCED/requests_practice/cookies.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-If the response from the server contains some cookies, you can access them.
+If response from server contains some cookies, you can access them
 """
 
 import requests
@@ -9,23 +9,12 @@ import requests
 
 r = requests.get('https://httpbin.org/get')
 
-#print(r.text)
-#print(r.headers)
-#print(r.cookies.get('example_cookie_name', None))
-
-# To send your own cookies to the server, you can use the cookies param.
-
 url = 'https://httpbin.org/cookies'
 cookies = {'cookies-are': 'working', 'cookies-allowed': 'True'}
 
 res = requests.get(url, cookies=cookies)
 print(res.text)
 
-# Cookies are returned in a RequestsCookieJar:
-# This acts like a dict but provides a more complete interface,
-# suitable for use over multiple domains or paths
-
-# Cookie jars can also be passed in to requests...
 cookie_jar = requests.cookies.RequestsCookieJar()
 
 cookie_jar.set('tasty_cookie', 'yum', domain='httpbin.org', path='/cookies')
