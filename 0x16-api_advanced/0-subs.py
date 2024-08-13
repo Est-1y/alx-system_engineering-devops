8dfa0a6a1058c92897f24cbdd8f4070cbbc2e83b
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-08-13 18:52:23 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
 """
-function querying the Reddit API.
+number of subscribers for a reddit
 """
+from requests import get
 
-import requests
 
 def number_of_subscribers(subreddit):
-    """querying Reddit API and returning no. of subs."""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
-    
-    try:
-        r = requests.get(url, headers=headers)
-        if r.status_code != 200:
-            return 0
-        r_json = r.json()
-        subs = r_json.get("data", {}).get("subscribers", 0)
-        return subs
-    except requests.exceptions.RequestException as e:
-        return 0
-    except ValueError:
-        return 0
+    """ querying subscriber count"""
+    if subreddit and type(subreddit) is str:
+        subscribers = 0
+        url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+        headers = {'user-agent': 'my-app/0.0.1'}
+        request = get(url, headers=headers)
+        if request.status_code == 200:
+            data = request.json()
+            subscribers = data.get('data', {}).get('subscribers', 0)
+        return subscribers
